32a70838c1c18cee336ba63f8d909c7ad26fb01d
Wang_by <1301027801@qq.com>
2020-10-07 01:32:15 +0800

    weather_api.py

--- python/homework_1/subtask2/weather_api.py
+++ python/homework_1/subtask2/weather_api.py
@@ -7,9 +7,25 @@ class WeatherAPI:
     api_url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
 
     def __init__(self, api_key):
+        self.api_key = api_key
         pass
 
-    def get_current_weather(self, lat, lon):
+    def get_current_weather(self,lat, lon):
+        lat = str(lat)
+        lon = str(lon)
+        api_url = 'https://api.weather.yandex.ru/v2/forecast?lat=' + lat + '&lon=' + lon + '&extra=true'
+        header = {'X-Yandex-API-Key': self.api_key }
+        weather = requests.get(api_url, headers=header).json()
+        name = weather['info']['tzinfo']['name']
+        current_time = weather['now_dt']
+        import time
+        t = time.strptime(current_time, "%Y-%m-%dT%H:%M:%S.%f%z")
+        current_time = time.strftime("%m/%d %H:%M:%S", t)
+        temp = weather['fact']['temp']
+        wind_speed = weather['fact']['wind_speed']
+        today = 'time zone:%s\ncurrent_time:%s\ntemp:%s°C\nwind_speed:%sM/C' % (name, current_time, temp, wind_speed)
+        return today
+
         """
             TODO: return a weather for current day
             {
@@ -23,7 +39,49 @@ class WeatherAPI:
         """
         pass
 
-    def get_forecast(self, lat, lon):
+    def get_forecast(self,lat, lon):
+        lat = str(lat)
+        lon = str(lon)
+        api_url = 'https://api.weather.yandex.ru/v2/forecast?lat=' + lat + '&lon=' + lon + '&extra=true&limit=7'
+        header = {'X-Yandex-API-Key': self.api_key}
+        weather = requests.get(api_url, headers=header).json()
+        forecast = ""
+
+        def tempa(x):
+            k = 0
+            t = 0
+            a = 0
+            while k <= 23:
+                try:
+                    t = t + weather['forecasts'][x]['hours'][k]['temp']
+                    a += 1
+                except IndexError:
+                    pass
+                k += 1
+            try:
+                temp = round(t / a, 2)
+            except ZeroDivisionError:
+                temp = "no information"
+            return temp
+
+        def pop_condition(x):
+            list = []
+            for c in range(24):
+                try:
+                    list.append(weather['forecasts'][x]['hours'][c]['condition'])
+                    conditions = max(set(list), key=list.count)
+                except IndexError:
+                    conditions = "no imformation"
+            return conditions
+
+        for i in range(7):
+            temp = str(tempa(i))
+            condition = pop_condition(i)
+            sunrise = weather['forecasts'][i]['sunrise']
+            sunset = weather['forecasts'][i]['sunset']
+            forecast = forecast + 'day:%s,temp:%s,condition:%s,sunrise/sunset:%s/%s\n' % (
+                i, temp, condition, sunrise, sunset)
+        return forecast
         """
             TODO: return a forecast for 7 days
             [{
