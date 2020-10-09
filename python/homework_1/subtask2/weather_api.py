a18d67a8e5e97bb2e260b7a5287b2b5d362a312b
Michael-tehc <71926248+Michael-tehc@users.noreply.github.com>
2020-10-09 14:29:26 +0000

    Removed walruses

--- python/homework_1/subtask2/weather_api.py
+++ python/homework_1/subtask2/weather_api.py
@@ -59,40 +59,26 @@ class WeatherAPI:
 
         parts_skipped = 'day_short', 'night_short'
 
-        # Возвращаем список из словарей,
-        # по одному словарю для каждого дня.
-        # Это одно большое выражение.
-        return [
-            # Это тьюпл, первые три элемента которого - просто
-            # для расчётов, а последний включается в результат
-            (
-                # Используем оператор-морж (walrus operator) для присвоения внутри выражения
-                parts := [
-                    (part['temp_avg'], part['condition'])
-                    for name, part in forecast['parts'].items()
-                    if name not in parts_skipped
-                ],
-                temps := [temp for temp, _ in parts],
-                conds := (cond for _, cond in parts), # Это ленивая операция
-                {
-                    'day': day,
-                    # Вся дичь с моржами нужна фактически только для того,
-                    # чтобы здесь получилось красивое вычисление среднего,
-                    # а также Counter(conds)
-                    'temp': sum(temps) / len(temps),
-                    'condition': Counter(conds).most_common(1)[0][0],
-                    'sunrise': forecast['sunrise'],
-                    'sunset': forecast['sunset']
-                }
-            )[-1]
-            for day, forecast in enumerate(sorted(data['forecasts'], key=lambda fc: fc['date']), 1)
-        ]
+        # Без дичи, ни единого моржа
+        forecasts = []
+        # Сортируем прогнозы по дню на всякий случай
+        for day, forecast in enumerate(sorted(data['forecasts'], key=lambda fc: fc['date']), 1):
+            # Получаем списки температур и эээ состояний погоды
+            # для каждой части дня
+            temperatures, conditions = zip(*(
+                (part['temp_avg'], part['condition'])
+                for name, part in forecast['parts'].items()
+                if name not in parts_skipped
+            ))
 
-if __name__ == '__main__':
-    # For testing only
+            one_day_forecast = {
+                'day': day,
+                'temp': sum(temperatures) / len(temperatures),
+                'condition': Counter(conditions).most_common(1)[0][0],
+                'sunrise': forecast['sunrise'],
+                'sunset': forecast['sunset']
+            }
 
-    from tokens import YANDEX_WEATHER_API_KEY
-    LOC = {'longitude': 37.3, 'latitude': 55.1}
+            forecasts.append(one_day_forecast)
 
-    api = WeatherAPI(YANDEX_WEATHER_API_KEY)
-    print(api.get_forecast(LOC['latitude'], LOC['longitude']))
+        return forecasts
