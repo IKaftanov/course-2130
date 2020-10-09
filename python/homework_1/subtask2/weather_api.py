180bb0704cc38601a85cfb5ecc940dd50d11ff46
Locoeka <guei20a@econ.msu.ru>
2020-10-09 19:53:15 +0400

    bot EasyWeather @Lococo_bot

--- python/homework_1/subtask2/weather_api.py
+++ python/homework_1/subtask2/weather_api.py
@@ -1,37 +1,52 @@
 import requests
 
+import datetime as dt
+import collections as coll
 
 class WeatherAPI:
-    # https://yandex.ru/dev/weather/doc/dg/concepts/forecast-test-docpage/#req-example
 
-    api_url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
+	 # https://yandex.ru/dev/weather/doc/dg/concepts/forecast-test-docpage/#req-example
+	 
 
-    def __init__(self, api_key):
-        pass
+	def __init__(self, api_key):
+		
+		self.url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
+		self.key = api_key
 
-    def get_current_weather(self, lat, lon):
-        """
-            TODO: return a weather for current day
-            {
-                'name': name (time zone name),
-                'current_time': current time,  # форматируйте время
-                'condition': condition (check the api description),
-                'temp': f'temp °C',
-                'wind_speed': f"{wind_speed} м/c"
-            }
+	def get_current_weather(self, lat, lon):
 
-        """
-        pass
+		data = requests.get(self.url.format(lat=lat, lon=lon), headers={'X-Yandex-API-Key': self.key}).json()
+		
+		current_weather = { 'time zone': data['info']['tzinfo']['name'],
+						   'current time': dt.datetime.fromtimestamp(data['now']).strftime('%m-%d %H:%M:%S'),
+						   'condition': data['fact']['condition'],
+						   'temp': '{temp} °C'.format(temp = data['fact']['temp']),
+						   'wind speed': '{wind_speed} м/c'.format(wind_speed = data['fact']['wind_speed']) }
 
-    def get_forecast(self, lat, lon):
-        """
-            TODO: return a forecast for 7 days
-            [{
-                    'day': i,
-                    'temp': sum(temps) / len(temps),  # температуры по частям дня
-                    'condition': most popular condition,  # самое популярное состояние по частям дня
-                    'sunrise': sunrise,
-                    'sunset': sunset
-            }...]
-        """
-        pass
+		return ' \n'.join(f'{k}: {v}' for k, v in current_weather.items())
+
+		
+	def get_forecast(self, lat, lon):
+
+		data = requests.get(self.url.format(lat=lat, lon=lon), headers={'X-Yandex-API-Key': self.key}).json()
+
+		def avg_part(parts):
+			sum = parts['day']['temp_avg']+parts['night']['temp_avg']+parts['evening']['temp_avg']+parts['morning']['temp_avg']
+			return sum/4
+			
+		def mostCommon(parts):
+			lst = [parts['day']['condition'],parts['night']['condition'],parts['evening']['condition'],parts['morning']['condition']]
+			return coll.Counter(lst).most_common(1)[0][0]
+	
+		def forecast(ind, day):
+			pred = { 'day': ind,
+                    'temp': '{temp} °C'.format(temp = avg_part(day['parts'])),  # температуры по частям дня
+                    'condition': mostCommon(day['parts']),  # самое популярное состояние по частям дня
+                    'sunrise/sunset': '{r}/{s}'.format(r = day['sunrise'], s = day['sunset']) }
+			return ', '.join(f'{k}: {v}' for k, v in pred.items())
+
+		res = ''
+		for ind, day in enumerate(data['forecasts']):
+			res = res +'\n\n' + forecast(ind ,day)
+
+		return res
--- python/homework_1/subtask2/weather_bot.py
+++ python/homework_1/subtask2/weather_bot.py
@@ -17,9 +17,9 @@ WEATHER_RETURN = 1
 
 
 def start(update, context):
-    """Send a message when the command /start is issued."""
 
-    update.message.reply_text('Welcome to my weather API bot')
+	update.message.reply_text('Welcome to my weather API bot!')
+	update.message.reply_text('Go to /weather ' )
 
 
 def get_weather_start(update, context):
@@ -29,22 +29,23 @@ def get_weather_start(update, context):
 
 
 def coordinates_handler(update, context):
-    reply_markup = telegram.ReplyKeyboardMarkup(
-        [[telegram.KeyboardButton(item) for item in WEATHER_CONVERSATION_COMMANDS]]
-    )
-    update.message.reply_text('Thank you. You location is captured', reply_markup=reply_markup)
-    # find and save location property in update
+	reply_markup = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton(item) for item in WEATHER_CONVERSATION_COMMANDS]])
+	update.message.reply_text('Thank you. You location is captured. Now select \n >forecast - to get the weather for next 7 days \n >current - to get current weather', reply_markup=reply_markup)
+	# find and save location property in update
     # you can use `context.user_data["location"]` to save user data
-    return 1
-
+	lat = update.message.location.latitude
+	lon = update.message.location.longitude
+	context.user_data["location"] = [lat, lon]
+	return 1
 
 def weather_handler(update, context):
+
     if update.message.text == WEATHER_CONVERSATION_COMMANDS[0]:
-        update.message.reply_text('current weather', reply_markup=telegram.ReplyKeyboardRemove())
+        update.message.reply_text(weather_api.get_current_weather(*context.user_data["location"]), reply_markup=telegram.ReplyKeyboardRemove())
         # TODO: send a current weather from yandex there
     elif update.message.text == WEATHER_CONVERSATION_COMMANDS[1]:
-        update.message.reply_text('forecast', reply_markup=telegram.ReplyKeyboardRemove())
-        # TODO: send a forecast of weather from yandex there
+        update.message.reply_text(weather_api.get_forecast(*context.user_data["location"]), reply_markup=telegram.ReplyKeyboardRemove())
+		# TODO: send a forecast of weather from yandex there
     return ConversationHandler.END
 
 
