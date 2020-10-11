cd9b90c71c60fe280b6e59746553916e076efcbe
Anastasia Nazarenko <anastasia.a.nazarenko@gmail.com>
2020-10-12 01:00:47 +0300

    Task 1

--- python/homework_1/subtask2/weather_api.py
+++ python/homework_1/subtask2/weather_api.py
@@ -1,42 +1,56 @@
 import requests
-from tokens import YANDEX_WEATHER_API_KEY
+
+import datetime as dt
+from collections import Counter
 
 class WeatherAPI:
     # https://yandex.ru/dev/weather/doc/dg/concepts/forecast-test-docpage/#req-example
 
-    api_url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
-
-    def __init__(self, api_key, api_url):
-        self.api_key = YANDEX_WEATHER_API_KEY
+    def __init__(self, api_key):
+        self.api_key = api_key
         self.api_url = 'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
 
     def get_current_weather(self, lat, lon):
-        api_url = f'https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}&extra=true'
-        response = requests.get(self.api_url, headers={'X-Yandex-API-Key': self.api_key})
-        print(response.json())
-        """
-            TODO: return a weather for current day
-            {
-                'name': name (time zone name),
-                'current_time': current time,  # форматируйте время
-                'condition': condition (check the api description),
-                'temp': f'temp °C',
-                'wind_speed': f"{wind_speed} м/c"
-            }
+        response = requests.get(self.api_url.format(lat=lat, lon=lon), headers={'X-Yandex-API-Key': self.api_key}).json()
 
-        """
-        pass
+        temp = response['fact']['temp']
+        wind_speed = response['fact']['wind_speed']
+
+        weather_dictionary = {'time zone': response['info']['tzinfo']['name'],
+                              'current time': dt.datetime.fromtimestamp(response['now']).strftime('%m/%d %H:%M:%S'),
+                              'condition': response['fact']['condition'],
+                              'temp': f'{temp} °C',
+                              'wind speed': f'{wind_speed} m/s'}
+
+        return ' \n'.join(f'{key}: {value}' for key, value in weather_dictionary.items())
+
 
     def get_forecast(self, lat, lon):
-        """
-            TODO: return a forecast for 7 days
-            [{
-                    'day': i,
-                    'temp': sum(temps) / len(temps),  # температуры по частям дня
-                    'condition': most popular condition,  # самое популярное состояние по частям дня
-                    'sunrise': sunrise,
-                    'sunset': sunset
-            }...]
-        """
-        pass
+        response = requests.get(self.api_url.format(lat=lat, lon=lon), headers={'X-Yandex-API-Key': self.api_key}).json()
+        bot_answer = ''
 
+        def avg_temp(parts):
+            sum_temps = (parts['day']['temp_avg'] + parts['night']['temp_avg'] +
+                         parts['evening']['temp_avg'] + parts['morning']['temp_avg'])
+            return sum_temps / 4
+
+        def most_popular_condition(parts):
+            most_popular = [parts['day']['condition'], parts['night']['condition'], parts['evening']['condition'],
+                            parts['morning']['condition']]
+            count = Counter(most_popular)
+            return count.most_common()[0][0]
+
+        def forecast(i, day):
+            r = day['sunrise']
+            s = day['sunset']
+            temp = avg_temp(day['parts'])
+            prediction = {'day': i,
+                    'temp': f'{temp} °C',
+                    'condition': most_popular_condition(day['parts']),
+                    'sunrise/sunset': f'{r}/{s}'}
+            return ', '.join(f'{key}: {value}' for key, value in prediction.items())
+
+        for i, day in enumerate(response['forecasts']):
+            bot_answer = bot_answer + '\n\n' + forecast(i, day)
+
+        return bot_answer
--- python/homework_1/subtask2/weather_bot.py
+++ python/homework_1/subtask2/weather_bot.py
@@ -20,6 +20,7 @@ def start(update, context):
     """Send a message when the command /start is issued."""
 
     update.message.reply_text('Welcome to my weather API bot')
+    update.message.reply_text('Enter /weather to see the weather')
 
 
 def get_weather_start(update, context):
@@ -32,19 +33,26 @@ def coordinates_handler(update, context):
     reply_markup = telegram.ReplyKeyboardMarkup(
         [[telegram.KeyboardButton(item) for item in WEATHER_CONVERSATION_COMMANDS]]
     )
-    update.message.reply_text('Thank you. You location is captured', reply_markup=reply_markup)
+    update.message.reply_text('Thank you. You location is captured. Now select \n >forecast - to get the weather for next 7 days \n >current - to get current weather', reply_markup=reply_markup)
     # find and save location property in update
     # you can use `context.user_data["location"]` to save user data
+    #context.user_data["location"] = [update.message.location.latitude, update.message.location.longitude]
+    lat = update.message.location.latitude
+    lon = update.message.location.longitude
+    context.user_data["location"] = [lat, lon]
     return 1
 
 
 def weather_handler(update, context):
+    #print(*context.user_data["location"])
+    #print(context.user_data["location"][0])
+    #print(context.user_data["location"][1])
     if update.message.text == WEATHER_CONVERSATION_COMMANDS[0]:
-        update.message.reply_text('current weather', reply_markup=telegram.ReplyKeyboardRemove())
-        # TODO: send a current weather from yandex there
+        update.message.reply_text(weather_api.get_current_weather(context.user_data["location"][0], context.user_data["location"][1]),
+                                  reply_markup=telegram.ReplyKeyboardRemove())
     elif update.message.text == WEATHER_CONVERSATION_COMMANDS[1]:
-        update.message.reply_text('forecast', reply_markup=telegram.ReplyKeyboardRemove())
-        # TODO: send a forecast of weather from yandex there
+        update.message.reply_text(weather_api.get_forecast(context.user_data["location"][0], context.user_data["location"][1]),
+                                  reply_markup=telegram.ReplyKeyboardRemove())
     return ConversationHandler.END
 
 
