bf916f930b965500fd4ef030f1cf576529e33746
ikuzmin404 <c255606@mail.ru>
2021-10-19 00:50:40 +0300

    HW2 done

--- year_2021/python/hw2/decorators.py
+++ year_2021/python/hw2/decorators.py
@@ -8,14 +8,13 @@ def call_controller(n_calls: int, time_interval: int):
     n_calls: количество возможнх вызовов
     time_interval: время в секундах
     ```
-
-    ```
-    @firewall(10, 20):
-    def foo():
-      pass
-    ```
     """
-
+    def foo():
+        pass
+        
+    for i in range(n_calls):
+        foo()
+        time.sleep(time_interval)
 
 def call_rectifier(func1, func2, func3, func4):
     """
@@ -23,4 +22,19 @@ def call_rectifier(func1, func2, func3, func4):
         1. Запускает функцию, если она завершается с ошибкой, то запускает следующую
         2. Если все функции завершились ошибкой (exception) -> вызвает exception `RuntimeError`
     """
-    pass
+    def runtimer():
+        try: 
+            return func1()
+        except:
+            try: 
+                return func2()
+            except:
+                try: 
+                    return func3()
+                except:
+                    try: 
+                        return func4()
+                    except:
+                        raise RuntimeError('Время вышло')
+    return runtimer
+        
