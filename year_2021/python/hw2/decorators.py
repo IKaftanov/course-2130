f5a7bcd5694a60813b53160ec74ca9f12ffab4fc
BorisenkoGeorgy <92270694+BorisenkoGeorgy@users.noreply.github.com>
2021-10-21 20:01:35 +0300

    Update decorators.py

--- year_2021/python/hw2/decorators.py
+++ year_2021/python/hw2/decorators.py
@@ -16,11 +16,37 @@ def call_controller(n_calls: int, time_interval: int):
     ```
     """
 
+    def decorator(func):
+        func.calls = 0
 
-def call_rectifier(func1, func2, func3, func4):
+        def wrapper(*args, **kwargs):
+            func.t = time.time()
+            if func.calls < n_calls:
+                func(*args, **kwargs)
+                if time.time() - func.t > time_interval:
+                    raise Exception('Функция превысила лимит по времени')
+            else:
+                raise Exception('Функция превысила лимит по вызовам')
+            func.calls += 1
+
+        return wrapper
+
+    return decorator
+
+import time
+
+def call_rectifier(*args):
     """
     Напишите декоратор который на вход принимает 4 функции и следует следующие логике:
         1. Запускает функцию, если она завершается с ошибкой, то запускает следующую
         2. Если все функции завершились ошибкой (exception) -> вызвает exception `RuntimeError`
     """
-    pass
+    for i in args:
+        try:
+            i()
+            return i
+        except:
+            continue
+    raise RuntimeError
+
+
