6adda8d7cbf1557b3966ef2149890aae30550c52
persono-avvito <47572546+persono-avvito@users.noreply.github.com>
2021-10-21 22:51:27 +0300

    Add files via upload

--- year_2021/python/hw2/custom_dts.py
+++ year_2021/python/hw2/custom_dts.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
-
 from typing import List, Any
 
 
 class CycledList:
+
     """
     Реализуйте список фиксированой длины, в котором новые элементы перезаписываются
 
@@ -22,14 +22,21 @@ class CycledList:
     [6, 2, 3, 4, 5]
     ```
     """
+
     def __init__(self, size: int):
         self._data = []
+        self.__size = size
 
-    def append(self, item):
+    def append(self, value):
+        if len(self._data) == self.__size:
+            self._data[0] = value
+        else:
+            self._data.append(value)
         pass
 
 
 class Fraction:
+
     """
     Написать класс чисел с бесконечной точностью. Дроби.
     Определите следующие операции:
@@ -44,27 +51,52 @@ class Fraction:
 
     """
 
-    def __init__(self, nominator, denominator):
-        self.nominator = nominator
-        self.denominator = denominator
+    def __init__(self, num, denom):
 
-    def __truediv__(self, other):
-        pass
+        def gcd(x,y):
+            if x > y:
+                if x%y!=0:
+                    val = gcd(y, x%y)
+                else:
+                    val = y
+            else: 
+                if y%x!=0:
+                    val = gcd(x, y%x)
+                else:
+                    val = x
+            return val
 
-    def __add__(self, other):
-        return Fraction(..., ...)
+        div = gcd(num, denom)
+        self.num = num//div
+        self.denom = denom//div
 
-    def __mul__(self, other):
-        pass
-
-    def __sub__(self, other: Fraction) -> Fraction:
-        pass
-
+    
     def __repr__(self):
-        return f'{self.nominator}/{self.denominator}'
+        return f"{self.num}/{self.denom}"
+        
+
+    def __eq__(self, second: Fraction) -> bool:
+        return (self.num==second.num)*(self.denom == second.denom)
+        
+
+    def __add__(self, second: Fraction) -> Fraction:
+        return Fraction(self.num*second.denom+self.denom*second.num, self.denom*second.denom)
+
+    def __sub__(self, second: Fraction) -> Fraction:
+        return Fraction(self.num*second.denom-self.denom*second.num, self.denom*second.denom)
+
+    def __mul__(self, second: Fraction) -> Fraction:
+        return Fraction(self.num*second.num, self.denom*second.denom)
+
+
+    def __truediv__(self, second: Fraction) -> Fraction:
+        return Fraction(self.num*second.denom, self.denom*second.num)
+
+
 
 
 class MyCounter:
+
     """
     Реализовать тип данных `Counter`, аналогично типу из `collections`
     https://docs.python.org/3/library/collections.html#collections.Counter
@@ -74,76 +106,109 @@ class MyCounter:
     """
 
     def __init__(self, iterable):
-        self._data = None
+        self._data = {x: iterable.count(x) for x in iterable}
 
-    def append(self, item):
+    def append(self, number):
+        if number in self._data:
+            self._data[number] = self._data[number] + 1
+        else:
+            self._data[number] = 1
         pass
 
-    def remove(self, item):
+    def remove(self, number):
+        del self._data[number]
         pass
 
 
 class Figure:
+
     def __init__(self, name):
         self.name = name
-
-    def perimeter(self):
-        return None
-
-    def square(self):
-        return None
 
     def __repr__(self):
         return f'Figure({self.name})'
 
 
+    def square(self):
+        return None
+
+    def perimeter(self):
+        return None
+
+
 class Square(Figure):
+
     """
     Реализуйте класс квадрат и два метода для него
     """
+
+    def __init__(self, length, width):
+        self.name = 'Square'
+        self.length = length
+        self.width = width
+    
+    def perimeter(self):
+        return 2*self.length + 2*self.width 
+
+    def square(self):
+        return self.length*self.width
+    
     pass
 
 
-class Container:
-    def __init__(self, data):
-        self.data = data
-
-    def __delitem__(self, key):
-        del self.data[key]
-
-    def __getitem__(self, item):
-        return self.data[item]
-
-    def append(self, item):
-        self.data.append(item)
-
-
+import json
 class PersistentList:
+
     """
     Реализуйте список где передаваемый список записывается в файл
     Любая операция удаления/добавления должна изменять файл
 
     Формат файла - json
     """
+
+    
     def __init__(self, iterable: List[Any], path_to_file: str):
+        self.path = path_to_file
+        self.data = iterable
+
+        with open(path_to_file, "w+") as f:
+            json.dump(iterable, f)
         pass
 
-    def append(self, item) -> None:
-        """add item to list"""
-
-    def __getitem__(self, index):
-        """ return item by index """
-        pass
-
-    def delete(self, index: int) -> None:
-        """ delete item by index
-
-            if index greater then length of list back to start and repeat
-                [1, 2, 3] -> delete(4) -> [1, 3]
-
-            if index lower then delete from end of list
-
-        """
 
     def __repr__(self):
+        return f'{self.data}'
+
+    def __getitem__(self, item):
+        item = item % len(self.data)
+        return self.data[item]
+
+    def __delitem__(self, key):
+        key = key % len(self.data)
+        del self.data[key]
+        with open(self.path, "w") as f:
+            json.dump(self.data, f)
         pass
+
+    def append(self, iterable: List[Any]):
+        self.data.append(iterable)
+        with open(self.path, "w") as f:
+            json.dump(self.data, f)
+        pass
+
+
+    def delete(self, index: int) -> None:
+
+        """ delete item by index
+            if index greater then length of list back to start and repeat
+                [1, 2, 3] -> delete(4) -> [1, 3]
+            if index lower then delete from end of list
+        """
+
+        index = index % len(self.data)
+        del self.data[index]
+        with open(self.path, "w") as f:
+            json.dump(self.data, f)
+        pass
+    
+
