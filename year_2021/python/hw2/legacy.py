c32395383edb69230cdc873e55f18fd0f16af375
BorisenkoGeorgy <92270694+BorisenkoGeorgy@users.noreply.github.com>
2021-10-21 20:03:41 +0300

    Update legacy.py

--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -4,29 +4,42 @@ def t1(number):
     Пример: number=21 тогда нужно вернуть 40
     Пример: -5 -> 0
     """
-    pass
-
+    if number%20 == 0:
+        return number
+    if number>0:
+        return number - number%20 + 20
+    else:
+        return number - number%(-20)
 
 def t2(string):
     """
     На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
     Пример: `abc abc abc` -> `cba cba cba`
     """
-    pass
-
+    str_s = string.split()
+    for i in range(len(str_s)):
+        str_s[i] = str_s[i][::-1]
+    str_s = ' '.join(str_s)
+    return str_s
 
 def t3(dictionary):
     """
     На вход подается словарь. Преобразуйте его в строку по следующему шаблону 'key: value; key: value' и так далее
     """
-    pass
+    str1 = str(dictionary)
+    str1 = str1.replace("'", "").replace(',', ';')
+    return str1[1:-1]
 
 
 def t4(string, sub_string):
     """
     проверить есть ли в строке инвертированная подстрока
     """
-    pass
+    string = string[::-1]
+    if sub_string in string:
+        return True
+    else:
+        return False
 
 
 def t5(strings):
