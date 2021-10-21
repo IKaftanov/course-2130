3f2b6b3aec75a8d99330ea58ab26d79243423a35
Malsar <petr2000k@mail.ru>
2021-10-21 22:51:09 +0300

    my changes

--- year_2021/python/hw2/decorators.py
+++ year_2021/python/hw2/decorators.py
@@ -1,14 +1,23 @@
 
 
 def call_controller(n_calls: int, time_interval: int):
+    import time
+    times = 0
+    time_now = time.time()
+    def decorator(func):
+        def decorated_func(*args, **kwargs):
+            while time.time() - time_now < float(time_interval):
+                while times < n_calls:
+                    func(*args, **kwargs)
+                    times += 1
+        return decorated_func
+    return decorator
     """
     Напишите функцию декоратор, которая ограничивает количество вызовов функции.
-
     ```
     n_calls: количество возможнх вызовов
     time_interval: время в секундах
     ```
-
     ```
     @firewall(10, 20):
     def foo():
@@ -18,9 +27,19 @@ def call_controller(n_calls: int, time_interval: int):
 
 
 def call_rectifier(func1, func2, func3, func4):
+    functions = [func1, func2, func3, func4]
+    exception_count
+    for _function in functions:
+        try:
+            _function()
+            return _function
+        except:
+            exception_count += 1
+            continue
+    if exception_count == 4:
+        raise RuntimeError
     """
     Напишите декоратор который на вход принимает 4 функции и следует следующие логике:
         1. Запускает функцию, если она завершается с ошибкой, то запускает следующую
         2. Если все функции завершились ошибкой (exception) -> вызвает exception `RuntimeError`
     """
-    pass
--- year_2021/python/hw2/legacy.py
+++ year_2021/python/hw2/legacy.py
@@ -1,18 +1,26 @@
 def t1(number):
+    x = number - number%20 + 20
+    return x
     """
     Поправьте код, чтобы возвращаемое значение было ближайшим сверху, кратным к 20
     Пример: number=21 тогда нужно вернуть 40
     Пример: -5 -> 0
     """
-    pass
 
 
 def t2(string):
+    x = string.split()
+    y = ""
+    for i in range(len(x)):
+        z = x[i][::-1]
+        y += " "
+        y += z
+    return y[1::]
     """
     На вход подается набор слов, разделенных пробелом, инвертируйте каждое слово.
     Пример: `abc abc abc` -> `cba cba cba`
     """
-    pass
+    
 
 
 def t3(dictionary):
@@ -23,13 +31,22 @@ def t3(dictionary):
 
 
 def t4(string, sub_string):
+    x = sub_string in string
+    return x
+    
     """
     проверить есть ли в строке инвертированная подстрока
     """
-    pass
+    
 
 
 def t5(strings):
+    x = []
+    for i in range(len(strings)):
+        if len(strings[i].split()) == 4:
+            if int(strings[i].split()[0])*int(strings[i].split()[1])*int(strings[i].split()[2]) == int(strings[i].split()[3]):
+                x.append(strings[i])
+    return x
     """
     На вход подается список строк,
     Отфильтруйте список строк, оставив только строки в формате: `x y z x*y*z`, где x,y,z - целые положительные числа
